80368233bec56c7157754e6186630fb3abf2f011
gedeokta28 <40822018+gedeokta28@users.noreply.github.com>
2024-09-23 18:40:29 +0700

    Update redirect to beyond

--- app/Http/Controllers/Guest/EventController.php
+++ app/Http/Controllers/Guest/EventController.php
@@ -11,51 +11,52 @@ class EventController extends Controller
 {
     public function index()
     {
-        $event = Event::where('event_active', 1)->first();
-        $eventList = Event::where('event_active', 1)->get();
-        // Format dates
-        if ($event) {
-            $initStartDate = \Carbon\Carbon::parse($event->event_start_date);
-            $initEndDate = \Carbon\Carbon::parse($event->event_end_date);
+        return redirect()->to('https://www.beyondliving.id/');
+        // $event = Event::where('event_active', 1)->first();
+        // $eventList = Event::where('event_active', 1)->get();
+        // // Format dates
+        // if ($event) {
+        //     $initStartDate = \Carbon\Carbon::parse($event->event_start_date);
+        //     $initEndDate = \Carbon\Carbon::parse($event->event_end_date);
 
-            $startDate = $initStartDate->format('j');
-            $endDate = $initEndDate->format('j');
-            $month = \Carbon\Carbon::parse($event->event_start_date)->format('F');
-            $year = \Carbon\Carbon::parse($event->event_start_date)->format('Y');
+        //     $startDate = $initStartDate->format('j');
+        //     $endDate = $initEndDate->format('j');
+        //     $month = \Carbon\Carbon::parse($event->event_start_date)->format('F');
+        //     $year = \Carbon\Carbon::parse($event->event_start_date)->format('Y');
 
-            $startDay = $initStartDate->format('l'); // Full day name (e.g., Monday)
-            $endDay = $initEndDate->format('l');     // Full day name (e.g., Wednesday)
-            // Combine into desired format
-            $formattedDateRange = "{$startDate}-{$endDate} {$month} {$year}";
-            $formattedDayRange = "{$startDay} to {$endDay}";
-            //
-            $eventTime = DateTime::createFromFormat('H:i', $event->event_time);
-        } else {
-            $formattedDateRange = '';
-        }
-        foreach ($eventList as $event) {
-            $initStartDate = \Carbon\Carbon::parse($event->event_start_date);
-            $initEndDate = \Carbon\Carbon::parse($event->event_end_date);
+        //     $startDay = $initStartDate->format('l'); // Full day name (e.g., Monday)
+        //     $endDay = $initEndDate->format('l');     // Full day name (e.g., Wednesday)
+        //     // Combine into desired format
+        //     $formattedDateRange = "{$startDate}-{$endDate} {$month} {$year}";
+        //     $formattedDayRange = "{$startDay} to {$endDay}";
+        //     //
+        //     $eventTime = DateTime::createFromFormat('H:i', $event->event_time);
+        // } else {
+        //     $formattedDateRange = '';
+        // }
+        // foreach ($eventList as $event) {
+        //     $initStartDate = \Carbon\Carbon::parse($event->event_start_date);
+        //     $initEndDate = \Carbon\Carbon::parse($event->event_end_date);
 
-            $startDate = $initStartDate->format('j');
-            $endDate = $initEndDate->format('j');
-            $month = \Carbon\Carbon::parse($event->event_start_date)->format('F');
-            $year = \Carbon\Carbon::parse($event->event_start_date)->format('Y');
+        //     $startDate = $initStartDate->format('j');
+        //     $endDate = $initEndDate->format('j');
+        //     $month = \Carbon\Carbon::parse($event->event_start_date)->format('F');
+        //     $year = \Carbon\Carbon::parse($event->event_start_date)->format('Y');
 
-            $startDay = $initStartDate->format('l');
-            $endDay = $initEndDate->format('l');
+        //     $startDay = $initStartDate->format('l');
+        //     $endDay = $initEndDate->format('l');
 
-            // Format tanggal untuk setiap event
-            $event->formattedDateRange = "{$startDate}-{$endDate} {$month} {$year}";
-            $event->formattedDayRange = "{$startDay} to {$endDay}";
+        //     // Format tanggal untuk setiap event
+        //     $event->formattedDateRange = "{$startDate}-{$endDate} {$month} {$year}";
+        //     $event->formattedDayRange = "{$startDay} to {$endDay}";
 
-            // Format waktu event
-            if ($event->event_time) {
-                $event->eventTime = DateTime::createFromFormat('H:i', $event->event_time);
-            }
-        }
+        //     // Format waktu event
+        //     if ($event->event_time) {
+        //         $event->eventTime = DateTime::createFromFormat('H:i', $event->event_time);
+        //     }
+        // }
 
-        return view('guests.index', compact('eventList', 'event', 'formattedDateRange', 'formattedDayRange', 'eventTime'));
+        // return view('guests.index', compact('eventList', 'event', 'formattedDateRange', 'formattedDayRange', 'eventTime'));
     }
 
     public function show($slug)
